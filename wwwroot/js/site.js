d8dbc4c38b1fbe34cff17a802708ac8c0975181e
Быков Егор <SEEGa1234@gmail.com>
2022-01-27 23:28:26 +0300

    Add maps and work to api yandex.maps

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+ymaps.ready(init);
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [54.88, 38.07],
+        zoom: 12,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+
+    var placemark = new ymaps.Placemark([54.88, 38.07], {
+        hintContent: 'Это хинт',
+        balloonContent: 'This is balloon'
+
+    });
+
+    myMap.geoObjects.add(placemark);
+
+}
