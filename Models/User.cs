16ae74eff7d9009358018f19c0667399fd30504b
Быков Егор <SEEGa1234@gmail.com>
2022-01-29 17:53:25 +0300

    Add search SportsObjects

---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+﻿namespace SportsGrounds.Models
+{
+   public enum TypePlayerBasket
+    {
+        PointGuard,
+        ShootingGuard,
+        SmallForward,
+        PowerForward,
+        Center
+
+    }
+    public enum Sports
+    {
+        BasketBall,
+        Football,
+        Volleyball,
+        Other
+    }
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string password { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? NickName { get; set; }
+        public TypePlayerBasket TypeBasketPlayer { get; set; }
+    }
+
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,6 +4,7 @@
 // Write your JavaScript code.
 
 ymaps.ready(init);
+
 function init() {
     var myMap = new ymaps.Map("map", {
         center: [54.88, 38.07],
@@ -12,6 +13,27 @@ function init() {
         behaviors: ['drag']
     });
 
+    var location = ymaps.geolocation.get();
+
+    var searchControl = new ymaps.control.SearchControl({
+        options: {
+            provider: 'yandex#search'
+        }
+    });
+    myMap.controls.add(searchControl);
+    searchControl.search('category_id:(77500452834)')
+    console.log(searchControl.getResultsCount());
+    
+    // Асинхронная обработка ответа.
+    location.then(
+        function (result) {
+            // Добавление местоположения на карту.
+            myMap.geoObjects.add(result.geoObjects)
+        },
+        function (err) {
+            console.log('Ошибка: ' + err)
+        }
+    );
     var placemark = new ymaps.Placemark([54.88, 38.07], {
         hintContent: 'Это хинт',
         balloonContent: 'This is balloon'
@@ -20,4 +42,17 @@ function init() {
 
     myMap.geoObjects.add(placemark);
 
+    var myGeocoder = ymaps.geocode("Ступино", {
+        
+        // Ограничение поиска видимой областью карты.
+      //  boundedBy: myMap.getBounds(),
+        // Жесткое ограничение поиска указанной областью.
+      // strictBounds: true
+    });
+    myGeocoder.then(function (res) {
+        myMap.geoObjects.add(res.geoObjects);
+
+    });
+
+   
 }
