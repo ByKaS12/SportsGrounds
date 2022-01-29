8365bc3cef61afe64d4d9fe7deeb4147b2267b4d
Быков Егор <SEEGa1234@gmail.com>
2022-01-29 17:53:25 +0300

    Add search SportsObjects

---
+++ Models/Map.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsGrounds.Models
+{
+    public enum TypeMaps
+    {
+        SportGround,
+        BasketGround,
+        FootballGround,
+        VolleyballGround
+    }
+    public class Map
+    {
+        public Guid Id { get; set; } 
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public TypeMaps Type { get; set; }
+        public string? Address { get; set; }
+
+
+    }
+}
