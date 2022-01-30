0a200375edae45d33ce76da3e0594d79eef6be6c
Быков Егор <SEEGa1234@gmail.com>
2022-01-30 15:04:57 +0300

    Complete realize CRUD func

---
+++ Interfaces/ICRUD.cs
@@ -0,0 +1,20 @@
+﻿using SportsGrounds.Models;
+
+namespace SportsGrounds.Interfaces
+{
+    public interface ICRUD
+    {
+        public void CreateMap(Map map);
+        public void UpdateMap(Map map);
+        public void DeleteMap(Map map);
+        public Map GetMap(Guid Id);
+        public List<Map> GetMaps();
+        public void CreateUser(User User);
+        public void UpdateUser(User User);
+        public void DeleteUser(User User);
+        public User GetUser(Guid Id);
+        public List<User> GetUsers();
+        
+        
+    }
+}
