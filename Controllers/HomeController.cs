3c0ca594dda36d432ecdb24a2ebbb476fd0c845d
Быков Егор <SEEGa1234@gmail.com>
2022-02-02 09:43:14 +0300

    Add new page and complete get obj from server

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,49 +12,8 @@ namespace SportsGrounds.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
-        public IActionResult Func()
-        {
-            string url = "https://search-maps.yandex.ru/v1/?text=category_id:(77500452834)&ll=38.078237,54.88628&spn=0.052069,0.050552&rspn=1&results=500&lang=ru_RU&apikey=b4a7ecbb-6ea1-40ba-b43d-03efb446b4a9";
-            GetRequest request = new GetRequest(url);
-            request.Run();
-            var response = request.Response;
-
-            var json = JObject.Parse(response);
-            var features = json["features"];
-            CRUD CRUD = new CRUD(_db);
-            try
-            {
-                foreach (var feature in features)
-                {
-                    var geometry = feature["geometry"];
-                    var coordinates = geometry["coordinates"];
-                    double Longitude = (double)coordinates[0];
-                    double Latitude = (double)coordinates[1];
-                    var properties = feature["properties"];
-                    string name = (string)properties["name"];
-                    var CompanyMetaData = properties["CompanyMetaData"];
-                    string address = (string)CompanyMetaData["address"];
-                    Map map = new();
-                    map.Longitude = Longitude;
-                    map.Latitude = Latitude;
-                    map.Address = address;
-                    map.Type = TypeMaps.SportGround;
-                    CRUD.CreateMap(map);
-                    
-
-                }
-            }
-            catch (Exception)
-            {
-            }
-
-            //TODO
-            return View("Index");
-        }
+       
     }
 }
